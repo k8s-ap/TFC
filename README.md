3d946a3177f72347ffbb8fe3096d06d847ee7d3d
Adrian Perez <perezjadrian@gmail.com>
2019-09-17 17:33:40 -0300

    se mejoro la vista de las funcionalidades que brinda el sistema

--- README.md
+++ README.md
@@ -2617,3 +2617,12 @@ In the future, we might start automatically compiling incompatible third-party m
 ## Something Missing?
 
 If you have ideas for more “How To” recipes that should be on this page, [let us know](https://github.com/facebookincubator/create-react-app/issues) or [contribute some!](https://github.com/facebookincubator/create-react-app/edit/master/packages/react-scripts/template/README.md)
+
+
+# FIX 
+
+## Tratamiento de errores
+
+### Cuando Create, Read, Update, Delete un registro de la BD Realtime Firebase.  
+
+Debo mostrar un mensaje de conexion a internet perdida o demasiado lenta.
--- controllers/solucionIoT/RootInstanceController.js
+++ controllers/solucionIoT/RootInstanceController.js
@@ -64,7 +64,6 @@ rootInstanceController.show = function(req, res) {
 };
 
 rootInstanceController.edit = function(req, res) {
-
     var asyncEditarRegistro = async() => {
         let registroAEditar = await RootInstance.findOne(req.params.id); // le envio por parametro la key del ultimo sensor cargado  
         console.log("Registro A Editar es: \n", registroAEditar);
@@ -74,9 +73,6 @@ rootInstanceController.edit = function(req, res) {
         });
     }
     asyncEditarRegistro();
-
-
-
 };
 
 rootInstanceController.delete = function(req, res) {
@@ -93,27 +89,20 @@ rootInstanceController.delete = function(req, res) {
 };
 
 rootInstanceController.update = function(req, res) {
-
-    // console.log("Actualizo los valores del id del sensor.");
     console.log("JSON recibido en mi metodo update:", req.body);
-    // res.redirect("/configuraciones/sensores/show/" + req.params.id);
-
     var asyncActualizo = async() => {
-        let set = {
+        /* let set = {
             address: req.body.address,
             apellidoNombre: req.body.apellidoNombre,
             email: req.body.email,
             telef: req.body.telef,
-        };
-
-        console.log("El pseudo JSON a subir a Firebase es:", set);
-
-        await RootInstance.findByIdAndUpdate(req.params.id, set); // le envio en el 1er parametro la key del sensor a actualizar y en el 2do parametro los datos a setear
-
+        }; */
+        let set = JSON.parse(JSON.stringify(req.body)); //convierto el body en string, luego a JSON y lo asigno a la variable set.(para ahorrarme lineas escribiendo clave y valor para cada propiedad)
+        console.log("El  JSON a subir a Firebase es:", set);
+        await RootInstance.findByIdAndUpdate(req.params.id, set); // le envio la key del registro a actualizar y  el dato a setear
         res.redirect("/solucionIoT/rootInstance/show/" + req.params.id);
     }
     asyncActualizo();
-
 };
 
 
